f3a965c6bce662291425f8424677a0530a38c18d
CISSOKO Kande <12401446@s30405.iutv.univ-paris13.fr>
2026-05-21 11:54:15 +0200

    hello world

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,8 +1,10 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		System.out.println("Ciao mondo !"); 
 		System.out.println("Hello World !");
+		
 	}
 
 }
